2c2c665403144a65272ec105795fe041790dd080
Amanda Perrone <amanda_degrande@yahoo.com.br>
2020-09-30 23:43:32 -0300

    solve bean

--- ac1-lab/src/main/java/com/example/ac1lab/Ac1LabApplication.java
+++ ac1-lab/src/main/java/com/example/ac1lab/Ac1LabApplication.java
@@ -1,13 +1,22 @@
 package com.example.ac1lab;
 
+import com.example.ac1lab.repository.PedidoRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Ac1LabApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ac1LabApplication.class, args);
+	}
+
+	// Acrescentado devido um erro na hora de iniciar o Spring-Boot
+	@Bean
+	public PedidoRepository pedidoRepository(){
+		return new PedidoRepository();
 	}
 
 }
